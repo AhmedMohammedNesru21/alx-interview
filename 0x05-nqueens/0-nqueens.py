13fdd0919cd267b3b2db68b86ad76224469aff83
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2023-05-26 01:19:03 +0000

    Task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,83 +1,56 @@
 #!/usr/bin/python3
-"""contains the nqueens code"""
 
-from sys import argv, exit
+'''
+N queens
+'''
 
-
-def queens(N, i, j, my_list):
-    """
-    place queens recursively
-    :param N: chessboard dimension
-    :param i: row
-    :param j: column
-    :param my_list:
-    :return:n queens
-    """
-    while i < N:
-        if validation(i, j, my_list):
-            my_list.append([i, j])
-            if j == N - 1:
-                print(my_list)
-                my_list.pop()
-            else:
-                queens(N, 0, j + 1, my_list)
-
-        i += 1
-
-    if len(my_list) > 0:
-        my_list.pop()
-
-    return
+from sys import argv
 
 
-def validation(i, j, my_list):
-    """
-    check if the position is valid
-    :param i: rows
-    :param j: columns
-    :param my_list: previously tested positions
-    :return: TRUE if valid, FALSE if not
-    """
-    rows = []
-    cols = []
-    diag1 = []
-    diag2 = []
-
-    for elem in my_list:
-        # rows and columns
-        rows.append(elem[0])
-        cols.append(elem[1])
-
-        # diagonals
-        diag1.append(elem[0] + elem[1])
-        diag2.append(elem[1] - elem[0])
-
-    if i in rows or j in cols:
-        return False
-
-    if i + j in diag1 or j - i in diag2:
-        return False
-
+def is_NQueen(cell: list) -> bool:
+    '''
+    True if N Queen, False if not
+    '''
+    row_number = len(cell) - 1
+    difference = 0
+    for index in range(0, row_number):
+        difference = cell[index] - cell[row_number]
+        if difference < 0:
+            difference *= -1
+        if difference == 0 or difference == row_number - index:
+            return False
     return True
 
 
-if __name__ == "__main__":
+def solve_NQeens(dimension: int, row: int, cell: list, outcome: list):
+    """
+    Return result of N Queens recusrivley
+    """
+    # Base case
+    if row == dimension:
+        print(outcome)
+    else:
+        for col in range(0, dimension):
+            cell.append(col)
+            outcome.append([row, col])
+            if (is_NQueen(cell)):
+                solve_NQeens(dimension, row + 1, cell, outcome)
+            cell.pop()
+            outcome.pop()
 
-    length = len(argv)
-    if length != 2:
-        print("Usage: nqueens N")
-        exit(1)
 
-    if argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
-
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+try:
     N = int(argv[1])
-
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    positions = []
-
-    queens(N, 0, 0, positions)
+except BaseException:
+    print('N must be a number')
+    exit(1)
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+else:
+    outcome = []
+    cell = 0
+    solve_NQeens(int(N), cell, [], outcome)

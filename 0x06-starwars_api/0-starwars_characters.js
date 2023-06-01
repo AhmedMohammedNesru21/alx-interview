1faedff3252d5302b176e07317eab607f6776526
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2023-06-01 22:30:45 +0000

    Task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,53 @@
 #!/usr/bin/node
 
-const mainUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const request = require('request');
 
-// Main Request
-request(mainUrl, function (error, response, body) {
-  if (error) {
-    console.log(error);
+const movieId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let people = [];
+const names = [];
+
+const requestCharacters = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
+
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
   } else {
-    const urlArray = JSON.parse(body).characters;
-    myAsyn(urlArray);
+    console.error('Error: Got no Characters for some reason');
   }
-});
+};
 
-// Sub request function
-function subRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (error2, response2, body2) {
-      if (error2) {
-        reject(error2);
-      } else {
-        resolve(JSON.parse(body2).name);
-      }
-    });
-  });
-}
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
 
-// Asynchronous function awaits for
-// sub request resolution after each iterations
-async function myAsyn (urlArray) {
-  for (const url of urlArray) {
-    const character = await subRequest(url);
-    console.log(character);
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
   }
-}
+};
+
+getCharNames();
